268bd8195da0a3103a98583cc4b2cc5240c18bae
anti-matt-er <m.burrows@live.co.uk>
2023-07-13 19:18:55 +0100

    Reduce Util to only used methods

--- SmartAutoAdvance/Util.cs
+++ SmartAutoAdvance/Util.cs
@@ -1,26 +1,11 @@
-using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Text;
-using Dalamud.Game;
-using Dalamud.Interface;
-using ImGuiNET;
 
 namespace SmartAutoAdvance {
-    internal static class Util {
-        internal static bool TryScanText(this SigScanner scanner, string sig, out IntPtr result) {
-            result = IntPtr.Zero;
-            try {
-                result = scanner.ScanText(sig);
-                return true;
-            } catch (KeyNotFoundException) {
-                return false;
-            }
-        }
-
+    public static class Util {
         private static unsafe byte[] ReadTerminatedBytes(byte* ptr) {
             if (ptr == null) {
-                return new byte[0];
+                return System.Array.Empty<byte>();
             }
 
             var bytes = new List<byte>();
@@ -32,19 +17,8 @@ namespace SmartAutoAdvance {
             return bytes.ToArray();
         }
 
-        internal static unsafe string ReadTerminatedString(byte* ptr) {
+        public static unsafe string ReadTerminatedString(byte* ptr) {
             return Encoding.UTF8.GetString(ReadTerminatedBytes(ptr));
-        }
-
-        internal static bool ContainsIgnoreCase(this string haystack, string needle) {
-            return CultureInfo.InvariantCulture.CompareInfo.IndexOf(haystack, needle, CompareOptions.IgnoreCase) >= 0;
-        }
-
-        internal static bool IconButton(FontAwesomeIcon icon, string id) {
-            ImGui.PushFont(UiBuilder.IconFont);
-            var ret = ImGui.Button($"{icon.ToIconString()}##{id}");
-            ImGui.PopFont();
-            return ret;
         }
     }
 }
